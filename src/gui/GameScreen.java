e5b60f98e319b05dcfd22f80f0ab7936a586ccff
Justin <36967116+jcx975@users.noreply.github.com>
2019-02-14 12:30:38 -0600

    Interface start

---
+++ src/gui/GameScreen.java
@@ -0,0 +1,13 @@
+
+package gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class GameScreen extends JFrame
+{
+	public GameScreen()
+	{
+		
+	}
+}
---
+++ src/gui/LobbyScreen.java
@@ -0,0 +1,13 @@
+
+package gui;
+
+import javax.swing.JFrame;
+
+@SuppressWarnings("serial")
+public class LobbyScreen extends JFrame
+{
+	public LobbyScreen()
+	{
+		
+	}
+}
