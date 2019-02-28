2f1c029fa8cbbe62583b486856dcdfee36e20bfd
jose <47397412+jose806@users.noreply.github.com>
2019-02-27 18:33:14 -0600

    Created game class
    Started the class for game

---
+++ src/Game.java
@@ -0,0 +1,30 @@
+
+public class Game {
+	Player main;
+	
+	public Game()
+	{
+		main = new Player();
+	}
+	
+	//This will add score from the main player("its the one on this device")
+	public void addScore(int in)
+	{
+		main.addScore(in);
+	}
+	/*
+	 * This will get the score from the player
+	 * to display in GUI, it returns an int
+	 */
+	public int getScore()
+	{
+		return main.getScore;
+	}
+	/*
+	 * This will check if the main player, this user 
+	 */
+	public boolean SuperSetCheck()
+	{
+		return main.setCheck();
+	}
+}
