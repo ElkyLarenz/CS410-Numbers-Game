8643968835680d59975576eb3681f817373a0480
Tim Jensen <timjensen757@gmail.com>
2019-03-03 21:17:21 -0600

    Fixed subset check

--- src/numbersgame/Game.java
+++ src/numbersgame/Game.java
@@ -349,22 +349,29 @@ public class Game {
     }
 
     // return true if a2 is a subset of a1
-    public boolean subsetCheck(int[] a1, int[] a2){
+    public boolean subsetCheck(int[] arr1, int[] arr2)
+    {
         int i = 0;
         int j = 0;
-        int n = a1.length;
-        int m = a2.length;
-
-        for (i = 0 ; i < n ; i++){
-            for (j = 0 ; j < m ; j++)
-                if(a1[i] == a2[j])
+        int m = arr1.length;
+        int n = arr2.length;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < m; j++)
+                if(arr2[i] == arr1[j])
                     break;
-                // if the above inner loop was not broken
-                // then a2[i] is not present in a1[]
+
+            /* If the above inner loop
+            was not broken at all then
+            arr2[i] is not present in
+            arr1[] */
             if (j == m)
                 return false;
         }
-        // if we reach here then all elements of a2 are in a1
+
+        /* If we reach here then all
+        elements of arr2[] are present
+        in arr1[] */
         return true;
     }
     //------------------------------------------------------------------------------------------------------
